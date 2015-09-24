e399ffd8519106e9849a5efae32cf8e2a44c5a74
mondial7 <mondial95@gmail.com>
2015-09-24 23:00:24 +0200

    changing something

--- client/Session.js
+++ client/Session.js
@@ -6,8 +6,34 @@ Meteor.subscribe("milk");
 
 Meteor.subscribe("Accounts");
 
+Meteor.subscribe("products");
+
+Meteor.subscribe("honey");
+
+Meteor.subscribe("strawberry");
+
+Meteor.subscribe("mushroom");
+
+Meteor.subscribe("blueberries");
+
+Meteor.subscribe("grapefruit");
+
+
+
 apple = new Mongo.Collection("apple");
 
 wine = new Mongo.Collection("wine");
 
 milk = new Mongo.Collection("milk");
+
+products = new Mongo.Collection("products");
+
+honey = new Mongo.Collection("honey");
+
+strawberry = new Mongo.Collection("strawberry");
+
+mushroom = new Mongo.Collection("mushroom");
+
+blueberries = new Mongo.Collection("blueberries");
+
+grapefruit = new Mongo.Collection("grapefruit");
--- server/Collection.js
+++ server/Collection.js
@@ -4,6 +4,18 @@ wine = new Mongo.Collection("wine");
 
 milk = new Mongo.Collection("milk");
 
+products = new Mongo.Collection("products");
+
+honey = new Mongo.Collection("honey");
+
+strawberry = new Mongo.Collection("strawberry");
+
+mushroom = new Mongo.Collection("mushroom");
+
+blueberries = new Mongo.Collection("blueberries");
+
+grapefruit = new Mongo.Collection("grapefruit");
+
 Meteor.publish("apple",function()
 {
 	if(this.userId)
@@ -34,4 +46,52 @@ Meteor.publish("Accounts",function()
 		return Meteor.users.find({_id:this.userId});
 	else
 		return null;
+});
+
+Meteor.publish("products",function()
+{
+	if(this.userId)
+		return products.find({$or:[{id:this.userId},{production:1,year:1,quantity:1}]});
+	else
+		return products.find({production:1,year:1,quantity:1});
+});
+
+Meteor.publish("honey",function()
+{
+	if(this.userId)
+		return honey.find({$or:[{id:this.userId},{production:1,year:1,quantity:1}]});
+	else
+		return honey.find({production:1,year:1,quantity:1});
+});
+
+Meteor.publish("strawberry",function()
+{
+	if(this.userId)
+		return strawberry.find({$or:[{id:this.userId},{production:1,year:1,quantity:1}]});
+	else
+		return strawberry.find({production:1,year:1,quantity:1});
+});
+
+Meteor.publish("mushroom",function()
+{
+	if(this.userId)
+		return mushroom.find({$or:[{id:this.userId},{production:1,year:1,quantity:1}]});
+	else
+		return mushroom.find({production:1,year:1,quantity:1});
+});
+
+Meteor.publish("blueberries",function()
+{
+	if(this.userId)
+		return blueberries.find({$or:[{id:this.userId},{production:1,year:1,quantity:1}]});
+	else
+		return blueberries.find({production:1,year:1,quantity:1});
+});
+
+Meteor.publish("grapefruit",function()
+{
+	if(this.userId)
+		return grapefruit.find({$or:[{id:this.userId},{production:1,year:1,quantity:1}]});
+	else
+		return grapefruit.find({production:1,year:1,quantity:1});
 });
